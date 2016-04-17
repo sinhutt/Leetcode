29bf887b02fa4cce92a374c2165b22c1d90c5c15
sinhutt <sinhutt@gmail.com>
2016-04-17 15:40:58 -0700

    Updated NimGame.java

--- NimGame.java
+++ NimGame.java
@@ -1,5 +1,11 @@
 public class Solution {
     public boolean canWinNim(int n) {
+    	/* If there are 4 stones, then you will never win the game:
+    	 * no matter 1, 2, or 3 stones you remove, the last stone will always be removed by your friend.
+    	 * If there are 5, 6, or 7 stones, then you will always win the game:
+    	 * you remove 1, 2, or 3 stones respectively, thereby converting to the case of 4 stones for your opponent.
+    	 * now the opponent will never win. etc.
+    	 */
         if (n % 4 == 0) return false;
         else return true;
     }
