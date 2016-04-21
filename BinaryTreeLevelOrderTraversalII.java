5601e0f7e8df512e3eda12faf1fce9f5aab1bbe6
sinhutt <sinhutt@gmail.com>
2016-04-20 20:43:03 -0700

    Created BinaryTreeLevelOrderTraversalII.java

---
+++ BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// DFS
+// public class Solution {
+//     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+//         LinkedList<List<Integer>> list = new LinkedList<List<Integer>>();
+//         addLevel(list, 0, root);
+//         return list;
+//     }
+
+//     private void addLevel(LinkedList<List<Integer>> list, int level, TreeNode node) {
+//         if (node == null) return;
+//         if (list.size()-1 < level) list.addFirst(new LinkedList<Integer>());
+//         list.get(list.size()-1-level).add(node.val);
+//         addLevel(list, level+1, node.left);
+//         addLevel(list, level+1, node.right);
+//     }
+// }
+
+// BFS
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> list = new LinkedList<List<Integer>>();
+        if (root == null) return list;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size(); // num elements at that level
+            List<Integer> levelList = new LinkedList<Integer>(); // list of elements at that level
+            for (int i = 0; i < levelNum; i++) {
+                TreeNode cur = queue.poll();
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+                levelList.add(cur.val);
+            }
+            list.add(0, levelList);
+        }
+        return list;
+    }
+}
