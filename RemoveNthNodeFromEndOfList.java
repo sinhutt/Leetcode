473588c74cb47a3c3a28eccf89d4bbb9ca9d1365
sinhutt <sinhutt@gmail.com>
2016-04-24 14:36:35 -0700

    Created RemoveNthNodeFromEndOfList.java

---
+++ RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // always have a dummy node when handling the edge case of head of the linked list
+        ListNode dummy = new ListNode(-1), slow = dummy, fast = dummy;
+        dummy.next = head;
+        // move fast so that there are n nodes in between slow and fast
+        for (int i = 1; i <= n + 1; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
+             
+
+
