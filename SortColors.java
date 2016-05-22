1a1bdbd2b51290226c8d69b5b8946fed66dfbe97
sinhutt <sinhutt@gmail.com>
2016-05-21 17:02:12 -0700

    Created SortColors.java

---
+++ SortColors.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public void sortColors(int[] nums) {
+        // 2-pass
+        // int count0 = 0, count1 = 0, count2 = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] == 0) count0++;
+        //     if (nums[i] == 1) count1++;
+        //     if (nums[i] == 2) count2++;
+        // }
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (i < count0) nums[i] = 0;
+        //     else if (i < count0 + count1) nums[i] = 1;
+        //     else nums[i] = 2;
+        // }
+        
+        // 1-pass
+        int p1 = 0, p2 = nums.length - 1, i = 0;
+        while (i <= p2) { // = sign handles cases like 1 1 1 0
+            if (nums[i] == 0) {
+                nums[i] = nums[p1];
+                nums[p1] = 0;
+                p1++;
+            }
+            if (nums[i] == 2) {
+                nums[i] = nums[p2];
+                nums[p2] = 2;
+                p2--;
+                // do not advance i pointer, b/c nums[i] need not be in the right position
+                // need to check its value in the next iteration
+                i--; 
+            }
+            i++;
+        }
+    }
+}
