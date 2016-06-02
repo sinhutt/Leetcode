d3d44fc6089649d2a138fd3d7ed512db8cc07bac
sinhutt <sinhutt@gmail.com>
2016-06-01 20:20:24 -0700

    Created IncreasingTripletSubsequence.java

---
+++ IncreasingTripletSubsequence.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        // start with two largest values
+        int small = Integer.MAX_VALUE, big = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n <= small) small = n; // update small if n is smaller than both
+            else if (n <= big) big = n; // update big only if greater than small but smaller than big
+            else return true; // return true if you find a number bigger than both
+        }
+        return false;
+    }
+}
