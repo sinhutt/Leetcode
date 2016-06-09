db9955582581afcc27d4ca2837aa8b587836c3a8
sinhutt <sinhutt@gmail.com>
2016-06-09 14:29:37 -0700

    Created IntersectionOfTwoArraysII.java

---
+++ IntersectionOfTwoArraysII.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        ArrayList<Integer> rs = new ArrayList<Integer>();
+        for (int i = 0; i < nums1.length; i++) {
+            if (map.containsKey(nums1[i]))
+                map.put(nums1[i], map.get(nums1[i]) + 1);
+            else
+                map.put(nums1[i], 1);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+                rs.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+            }
+        }
+        int[] result = new int[rs.size()];
+        for (int i = 0; i < rs.size(); i++)
+            result[i] = rs.get(i);
+        return result;
+    }
+}
