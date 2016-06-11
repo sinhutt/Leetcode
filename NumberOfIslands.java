633f91c3e4a179c0e63ea4621040045c3d6fc81d
sinhutt <sinhutt@gmail.com>
2016-06-11 16:47:27 -0700

    Created NumberOfIslands.java

---
+++ NumberOfIslands.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    DFSMarking(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    private void DFSMarking(char[][] grid, int i, int j) {
+        // don't forget grid[i][j] != '1'
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        DFSMarking(grid, i + 1, j);
+        DFSMarking(grid, i - 1, j);
+        DFSMarking(grid, i, j + 1);
+        DFSMarking(grid, i, j - 1);
+    }
+}
