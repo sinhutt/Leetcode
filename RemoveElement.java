c9a0513cdc256c24f7b0650ac839161777f55eca
sinhutt <sinhutt@gmail.com>
2016-06-28 01:01:24 -0700

    Updated RemoveElement.java

--- RemoveElement.java
+++ RemoveElement.java
@@ -1,12 +1,23 @@
 public class Solution {
     public int removeElement(int[] nums, int val) {
-        int m = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[m]= nums[i];
-                m++;
+        // int m = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] != val) {
+        //         nums[m]= nums[i];
+        //         m++;
+        //     }
+        // }
+        // return m;
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            if (nums[l] == val) {
+                nums[l] = nums[r];
+                nums[r] = val;
+                r--;
+            } else {
+                l++;
             }
         }
-        return m;
+        return l;
     }
 }
