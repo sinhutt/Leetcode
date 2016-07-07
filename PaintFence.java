44a9a0522f72b55e6e58aebc56736ef6a44fc30c
sinhutt <sinhutt@gmail.com>
2016-07-07 00:58:13 -0700

    Created PaintFence.java

---
+++ PaintFence.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int numWays(int n, int k) {
+        if (n == 0) return 0;
+        if (n == 1) return k;
+        int diffCol = k * (k-1);
+        int sameCol = k;
+        for (int i = 2; i < n; i++) {
+            int tmp = diffCol;
+            diffCol = (diffCol + sameCol) * (k - 1);
+            sameCol = diffCol;
+        }
+        return diffCol + sameCol;
+    }
+}
