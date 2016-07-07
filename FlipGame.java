516f94bbe135fb65285d7bdf0624318b28c4098d
sinhutt <sinhutt@gmail.com>
2016-07-07 01:16:26 -0700

    Created FlipGame.java

--- FlipGame.java
+++ FlipGame.java
@@ -3,7 +3,7 @@ public class Solution {
         List<String> rs = new ArrayList<String>();
         for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i-1) == '+' && s.charAt(i) == '+') 
-                rs.add(s.substring(0, i-1) + "--" + s.substring(i+1, s.length));
+                rs.add(s.substring(0, i-1) + "--" + s.substring(i+1, s.length()));
         }
         return rs;
     }
