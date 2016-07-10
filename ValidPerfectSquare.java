2859bc8ee55acc36e6ab87396d9cffa448181b9b
sinhutt <sinhutt@gmail.com>
2016-07-09 19:48:53 -0700

    Created ValidPerfectSquare.java

---
+++ ValidPerfectSquare.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        /**
+         * This is a math problem：
+         * 1 = 1
+         * 4 = 1 + 3
+         * 9 = 1 + 3 + 5
+         * 16 = 1 + 3 + 5 + 7
+         * 25 = 1 + 3 + 5 + 7 + 9
+         * 36 = 1 + 3 + 5 + 7 + 9 + 11
+         * ....
+         * so 1+3+...+(2n-1) = (2n-1 + 1)n/2 = nn
+         */ 
+         int i = 1;
+         while (num > 0) {
+             num -= i;
+             i += 2;
+         }
+         return num == 0;
+    }
+}
